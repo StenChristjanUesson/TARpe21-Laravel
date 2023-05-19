322f8951b3a2e0d1b8c67e82525003c15aa4772f
KaarelNoole <kaarelnoole@gmail.com>
2023-05-19 12:59:29 +0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks', [TasksController::class, 'index'])->name('task.index');
+Route::post('/tasks', [TasksController::class, 'create'])->name('task.create');
+Route::delete('/tasks/{id}', [TasksController::class, 'delete'])->name('task.delete');
