e30edb41460bb24f88e74c174b655ea04eed2eb0
KaarelNoole <kaarelnoole@gmail.com>
2023-06-08 10:05:06 +0300

    PasswordUpdateTests.php updated

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -2,50 +2,31 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
-class PasswordUpdateTest extends TestCase
+class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_password_can_be_updated(): void
+    public function test_registration_screen_can_be_rendered(): void
     {
-        $user = User::factory()->create();
+        $response = $this->get('/register');
 
-        $response = $this
-            ->actingAs($user)
-            ->from('/profile')
-            ->put('/password', [
-                'current_password' => 'password',
-                'password' => 'new-password',
-                'password_confirmation' => 'new-password',
-            ]);
-
-        $response
-            ->assertSessionHasNoErrors()
-            ->assertRedirect('/profile');
-
-        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+        $response->assertStatus(200);
     }
 
-    public function test_correct_password_must_be_provided_to_update_password(): void
+    public function test_new_users_can_register(): void
     {
-        $user = User::factory()->create();
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
 
-        $response = $this
-            ->actingAs($user)
-            ->from('/profile')
-            ->put('/password', [
-                'current_password' => 'wrong-password',
-                'password' => 'new-password',
-                'password_confirmation' => 'new-password',
-            ]);
-
-        $response
-            ->assertSessionHasErrorsIn('updatePassword', 'current_password')
-            ->assertRedirect('/profile');
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
     }
 }
